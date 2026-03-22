939860b5d52412ded272fef3015282d029fb00f7
TheAaronHylands <160644484+TheAaronHylands@users.noreply.github.com>
2026-03-22 15:33:52 -0400

    Files updated after closing VisualStudio

--- obj/Release/net9.0/Hierarchanizer.AssemblyInfo.cs
+++ obj/Release/net9.0/Hierarchanizer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hierarchanizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1fa097273e58b21c3d364c6d8d4882abaec757")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b429fe4dd3b45f86b0e43f982eeb58d684d3bedf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hierarchanizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hierarchanizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
